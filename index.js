ebaf5b0554d03c89312230cf5799a27c8526c967
Slavril <slavril@mail.com>
2022-03-07 16:18:10 +0700

    all

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
-
 import express from 'express'
 import http from "http"
 var app = express();
 const server = http.createServer(app);
 
+const port = process.env.PORT || 3000
+
 import { SocketService } from './app/service/Socket.service.js'
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log('Server đang chay tren cong 3000');
   const socketService = new SocketService()
   socketService.start(server)
